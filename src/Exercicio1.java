a97a9e9768e3771eff0576641532587fe8baa67c
Mariana Neves Monteiro <1196702346@acd.grupotiradentes.com>
2020-02-07 21:05:52 -0300

    soma

---
+++ src/Exercicio1.java
@@ -0,0 +1,16 @@
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int a = 10, b= 20;
+    
+		
+		System.out.println(a + b);
+    
+
+	
+	}
+
+}
